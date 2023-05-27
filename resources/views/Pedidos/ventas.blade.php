e7941c4107f42c0330eeb5aa7936b376429ec496
carloseduvillegas <hacker.ceva@gmail.com>
2023-05-27 16:55:07 -0400

    Quitando chartjs

--- resources/views/Pedidos/ventas.blade.php
+++ resources/views/Pedidos/ventas.blade.php
@@ -79,74 +79,72 @@
 
 @section('js')
     <script> console.log('Hi!'); </script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/{version}/Chart.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/{version}/chartjs-plugin-datalabels.min.js"></script>
+    
 
     <script>
-        $(document).ready(function(){
-            var nombres = {!! json_encode($producto) !!};
-            var cantidades = {!! json_encode($cantidad) !!};
-            // var datosInvertidos = nombres.map(function(nombre, index) {
-            //     return {
-            //         y: nombre,
-            //         x: cantidades[index]
-            //     };
-            // });
-            const ctx = document.getElementById('myChart').getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: nombres,
-                    datasets:[{
-                        label:'Productos vendidos del dia',
-                        data: cantidades,
-                        backgroundColor:[
-                            '#F95A1E',
-                            '#F9A31E',
-                            '#F9DE1E',
-                            '#E2F91E',
-                            '#C4F91E',
-                            '#1EF999',
-                            '#1EF9D8',
-                            '#1EF6F9',
-                            '#1ED1F9',
-                            '#1E9FF9',
-                            '#601EF9',
-                            '#991EF9',
-                            '#C41EF9',
-                            '#F91E85',
-                            '#F91E1E',
-                        ],
-                        borderWidth: 0,
+        // $(document).ready(function(){
+        //     var nombres = {!! json_encode($producto) !!};
+        //     var cantidades = {!! json_encode($cantidad) !!};
+        //     // var datosInvertidos = nombres.map(function(nombre, index) {
+        //     //     return {
+        //     //         y: nombre,
+        //     //         x: cantidades[index]
+        //     //     };
+        //     // });
+        //     const ctx = document.getElementById('myChart').getContext('2d');
+        //     var myChart = new Chart(ctx, {
+        //         type: 'bar',
+        //         data: {
+        //             labels: nombres,
+        //             datasets:[{
+        //                 label:'Productos vendidos del dia',
+        //                 data: cantidades,
+        //                 backgroundColor:[
+        //                     '#F95A1E',
+        //                     '#F9A31E',
+        //                     '#F9DE1E',
+        //                     '#E2F91E',
+        //                     '#C4F91E',
+        //                     '#1EF999',
+        //                     '#1EF9D8',
+        //                     '#1EF6F9',
+        //                     '#1ED1F9',
+        //                     '#1E9FF9',
+        //                     '#601EF9',
+        //                     '#991EF9',
+        //                     '#C41EF9',
+        //                     '#F91E85',
+        //                     '#F91E1E',
+        //                 ],
+        //                 borderWidth: 0,
                         
-                    }]
-                },
-                options: {
-                    indexAxis: 'x',
-                    scales: {
-                        y: {
-                            beginAtZero: true,
-                        },
-                    },
-                }
-            });
+        //             }]
+        //         },
+        //         options: {
+        //             indexAxis: 'x',
+        //             scales: {
+        //                 y: {
+        //                     beginAtZero: true,
+        //                 },
+        //             },
+        //         }
+        //     });
 
-            console.log(cData)
+        //     console.log(cData)
 
-            // Ajustar el tamaño del gráfico al contenedor
-            function resizeChart() {
-                var container = document.getElementById('myChart').parentNode;
-                var canvas = document.getElementById('myChart');
-                canvas.width = container.offsetWidth;
-                canvas.height = container.offsetHeight;
-                myChart.resize();
-            }
+        //     // Ajustar el tamaño del gráfico al contenedor
+        //     function resizeChart() {
+        //         var container = document.getElementById('myChart').parentNode;
+        //         var canvas = document.getElementById('myChart');
+        //         canvas.width = container.offsetWidth;
+        //         canvas.height = container.offsetHeight;
+        //         myChart.resize();
+        //     }
 
-            // Ajustar el tamaño del gráfico cuando se redimensiona la ventana
-            window.addEventListener('resize', resizeChart);
-            resizeChart(); // Ajustar el tamaño inicial del gráfico
-        });
+        //     // Ajustar el tamaño del gráfico cuando se redimensiona la ventana
+        //     window.addEventListener('resize', resizeChart);
+        //     resizeChart(); // Ajustar el tamaño inicial del gráfico
+        // });
     </script>
     
 
